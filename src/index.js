7b94bd3d7736e584ab7629fcf42d2a48615d58d5
Nataly Baranova <borashek@inbox.ru>
2024-08-07 12:47:14 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let number;
+  if (n < 0) {
+    number = n * (-1);
+  } else {
+    number = n;
+  }
+  return number.toString().split('').reverse().join('');
 }
